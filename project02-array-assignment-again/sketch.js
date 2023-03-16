bc4d084f37407015088bea5184c4d9128eced21f
ethan-sparrow <ethan@kimble.ca>
2023-03-16 15:20:20 -0600

    almost done rings

--- project02-array-assignment-again/sketch.js
+++ project02-array-assignment-again/sketch.js
@@ -1,26 +1,55 @@
-// Project Title
-// Your Name
-// Date
+// Array assignment (2nd try)
+// Ethan/Sparrow Kimble
+// March 15th, 2023
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 let rings = [];
+let globalX = 0;
+let globalY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   normalMaterial();
-  spawnRing(0, 0, 100)
+  spawnRing(50, 0, 100);
+  spawnRing(0, 100, 0)
 }
 
 function draw() {
   background(220);
-  
-  drawRings();
+  playerInput();
+  for(let i = 0; i < rings.length; i++) {
+    moveRings(rings[i]);
+    drawRings();
+  }
+}
+
+function playerInput(){
+  if (keyIsDown(LEFT_ARROW)) {
+    globalX += 3;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    globalX -= 3;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    globalY += 3;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    globalY -= 3;
+  }
+}
+
+function moveRings(ring) {
+  ring.z += 2;
+  let x = globalX + ring.x;
+  let y = globalY + ring.y;
+  translate(x, y, ring.z);
 }
 
 function drawRings() {
-  for
+  
+  torus (100, 20, 50, 50);
 }
 
 function spawnRing(ringX, ringY, ringZ) {
